2275286d5c4d34529d16e3e2b43a506e667fee70
Joel G <wanstiller@gmail.com>
2022-09-29 23:27:42 -0500

    Update

--- includes/config.php
+++ includes/config.php
@@ -2,7 +2,7 @@
 	define('DB_SERVER', 'localhost');
 	define('DB_USER', 'root');
 	define('DB_PASS', '');
-	define('DB_NAME', 'blogphp');
+	define('DB_NAME', 'java');
 	$con=@mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
     if(!$con){
         @die("<h2 style='text-align:center'>Imposible conectarse a la base de datos! </h2>".mysqli_error($con));
@@ -10,4 +10,3 @@
     if (@mysqli_connect_errno()) {
         @die("Conexión falló: ".mysqli_connect_errno()." : ". mysqli_connect_error());
     }
-?>
